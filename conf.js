98b763763ae4a1e93469ec9a0a5c06146c07a13f
wissembendaly <wissembendaly@insat.u-carthage.tn>
2022-06-08 01:26:39 +0100

    tester

--- conf.js
+++ conf.js
@@ -5,6 +5,14 @@ exports.config = {
     allScriptsTimeout: 60000,
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+            args: ['--verbose'],
+            binary: 'C:/Program Files/Mozilla Firefox/firefox.exe' //Provide binary location to avoid potential binary not found errors 
+       //Need to start cmd via admin mode to avoid permission error
+        }
+   },  
     specs: ['spec.js'],
     onPrepare: function() {
       // Add a screenshot reporter and store screenshots to `/Reports/screenshots`:
